1165d01756a3d1bed4c4657b10b349b9b877991c
JP <joao.rocha@vnda.com.br>
2023-06-02 19:13:10 -0300

    site com pergunta

--- src/js/components/home.js
+++ src/js/components/home.js
@@ -1,89 +1,6 @@
 
 
 const Home = {
-  setIconsSwiper: () => {
-    const swiper = new Swiper('.swiper-icons', {
-      // Optional parameters
-      direction: 'vertical',
-      loop: true,
-      direction: 'horizontal',
-      // If we need pagination
-      pagination: {
-        el: '.swiper-pagination',
-      },
-      // Navigation arrows
-      breakpoints: {
-        0: {
-          slidesPerView: 1,
-          spaceBetween: 0
-        },
-        565: {
-          slidesPerView: 3,
-          spaceBetween: 5,
-        },
-        768: {
-          slidesPerView: 4,
-          spaceBetween: 5,
-        },
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-  },
-  setStructureSwiper: () => {
-    const swiper = new Swiper('.swiper-structure', {
-      // Optional parameters
-      loop: true,
-      slidesPerView: 1,
-      direction: 'horizontal',
-      autoplay: {
-        delay: 4000
-      },
-      // If we need pagination
-      pagination: {
-        el: '.swiper-pagination',
-      },
-      // Navigation arrows
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-  },
-  setTeamSwiper: () => {
-    const swiper = new Swiper('.swiper-team', {
-      // Optional parameters
-      direction: 'horizontal',
-      autoplay: {
-        delay: 3000
-      },
-      breakpoints: {
-        0: {
-          slidesPerView: 1.2,
-          spaceBetween: 20,
-        },
-        565: {
-          slidesPerView: 3,
-          spaceBetween: 5,
-        },
-        768: {
-          slidesPerView: 4,
-          spaceBetween: 25,
-        },
-      },
-      // If we need pagination
-      pagination: {
-        el: '.swiper-pagination',
-      },
-      // Navigation arrows
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-  },
   accordions: () => {
     const accordionHeaders = document.querySelectorAll(".accordion-header");
 
@@ -119,10 +36,7 @@ const Home = {
 
   init: function () {
     const _this = this;
-    _this.setIconsSwiper();
-    _this.setStructureSwiper();
     _this.accordions();
-    _this.setTeamSwiper();
 
     _this.animateElements();
   },
--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,3 @@
 import Home  from  './components/home.js';
     
 Home.init();
-
-console.log(
-    '%cSorria +',
-    'color: #bdfb7f; font-size: 30px; font-family: "Montserrat", sans-serif; font-weight: 800;'
-);
